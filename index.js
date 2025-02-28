aa19073591af75c1564b808fb9ac01aa537c32ec
Baazel <xshadosx@aol.com>
2025-02-27 23:05:19 -0300

    Implementacion de token JWT

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 const express = require('express')
 const { exec } = require("child_process");
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080;
+const secret = process.env.SECRET_KEY;
+const secretusr = process.env.SECRET_USER;
+const secretpass = process.env.SECRET_PASS;
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    var cmdCommand = req.body.command;
-    exec(cmdCommand, (error, stdout, stderr) => {
-        var response = {
-            error: error?error.message:"",
-            stderr: stderr?stderr:"",
-            stdout: stdout?stdout:""
-        }
+app.post('/login',(req, res) => {
+    var requsr = req.body.user;
+    var reqpass = req.body.pass;
+    if (requsr==secretusr && reqpass == secretpass){
+        var payload = { user: { username: requsr } };
+        var token = jwt.sign(payload, secret, { expiresIn: '5m' });
+        res.send(token)
+    }
+    else{
+        res.status(400).send('Usuario invalido')
+    }
+})
 
-        if (error) {
-            console.log(`error: ${error.message}`);
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        if (stdout) {
-            console.log(`stdout: ${stdout}`);
-        }
-        res.send(response) 
+app.post('/', (req, res) => {
+    var authHeader = req.headers['authorization'];
+    var secretKey = req.headers['secret'];
+    var token = authHeader && authHeader.split(' ')[1];
+
+    // Sin token
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        //Token invalido
+        if (err) return res.sendStatus(403);
+        var cmdCommand = req.body.command;
+        exec(cmdCommand, (error, stdout, stderr) => {
+            var response = {
+                error: error?error.message:"",
+                stderr: stderr?stderr:"",
+                stdout: stdout?stdout:""
+            }
+
+            if (error) {
+                console.log(`error: ${error.message}`);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+            }
+            if (stdout) {
+                console.log(`stdout: ${stdout}`);
+            }
+            res.send(response) 
+        });
     });
 })
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`)
+  console.log(`ServerCMD utilizando puerto: ${port}`)
 })
